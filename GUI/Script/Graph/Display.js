c55eaf719293d5fac7a2791f3045b00dc46198da
shxll <135678100+shxll@users.noreply.github.com>
2024-06-29 19:21:15 -0400

    Display.js

---
+++ GUI/Script/Graph/Display.js
@@ -0,0 +1,10 @@
+function OpenNav(){
+    document.getElementById("Hidden").style.display="block";
+    document.getElementById("New").style.display="none";
+    document.getElementById("Open").style.display="none";
+}
+function OpenNav2(){
+    document.getElementById("Hidden2").style.display="block";
+    document.getElementById("New").style.display="none";
+    document.getElementById("Open").style.display="none";
+}
