f868d4f2b49487e1eba5628059172b485f7d0ad3
shxll <135678100+shxll@users.noreply.github.com>
2024-06-29 19:19:14 -0400

    Author.js

---
+++ GUI/Script/Author.js
@@ -0,0 +1,11 @@
+function English(){
+    alert("This tool has been made by Luca Garofalo (Lucksi) in Sicily a small Italian island Thank you for have>
+}
+
+function Italiano(){
+    alert("Questo tool è stato creato da Luca Garofalo (Lucksi) in Sicilia Grazie per aver scaricato il mio tool>
+}
+
+function French(){
+    alert("Cette tool A été créé par Luca Garofalo (Lucksi) en Sicilia a petité Italien ile Merci beacoup pour a>
+}
