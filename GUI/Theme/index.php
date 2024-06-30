d871675b2fa65b6d36a47428eca40f2635940db4
shxll <135678100+shxll@users.noreply.github.com>
2024-06-29 21:14:00 -0400

    index.php

---
+++ GUI/Theme/index.php
@@ -0,0 +1,8 @@
+<?php
+/*ORIGINAL CREATOR: Sharllan Paulino (Shxll)
+AUTHOR: Sharllan Paulino (Shxll)
+Copyright (C) 2021-2023 Shxll <sharllans2@gmail.com>
+License: GNU General Public License v3.0*/ 
+
+require("Actions/Login_Controller.php");
+?>
